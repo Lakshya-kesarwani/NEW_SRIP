36326a378c5154dd56a94318613f8331218024a5
Lakshya Kesarwani <ansh1.kesari2005@gmail.com>
2025-01-05 10:42:27 +0530

    edited

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash,jsonify
 from flask_mysqldb import MySQL
-import MySQLdb.cursors
+from mysqlclient import MySQLdb.cursors
 import bcrypt
 import os 
 
